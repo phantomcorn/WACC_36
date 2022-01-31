a4eb321495a831849a876ab2e7757f2c17a09f20
Phantakorn Prarusudamkerng <pp1120@ic.ac.uk>
2022-01-31 12:05:37 +0000

    feat: add main func to parse in input to compiler + add visitor class to help with semantic analysis

--- src/MyVisitor.kt
+++ src/MyVisitor.kt
@@ -1,2 +1,18 @@
-class MyVisitor {
+package myvisitor
+
+import antlr.*
+import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.tree.RuleNode
+
+class MyVisitor : WACCParserBaseVisitor<Void>() {
+
+    override fun visitProg(ctx: WACCParser.ProgContext): Void? {
+        println("Program visit")
+        return null;
+    }
+
+    override fun visitFunc(ctx: WACCParser.FuncContext): Void? {
+        println("There's a function in this tree");
+        return null;
+    }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,2 +1,26 @@
-class main {
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import antlr.*;
+
+import myvisitor.*;
+
+fun main() {
+
+    val input = CharStreams.fromStream(System.`in`);
+
+    val lexer = WACCLexer(input);
+
+    val tokens = CommonTokenStream(lexer);
+
+    val parser = WACCParser(tokens);
+
+    val tree = parser.prog();
+
+    println(tree.toStringTree(parser));
+
+    println("=====");
+    val visitor = MyVisitor();
+    visitor.visit(tree);
+    println("=====");
 }
