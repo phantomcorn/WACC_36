47be98967dcafcb41dd703936258c59b6c529dd1
Charles Loveman <charlie@lovemans.co.uk>
2022-02-07 20:48:44 +0000

    feat: add unfinished declaration definition

---
+++ src/stat/Declaration.kt
@@ -0,0 +1,25 @@
+package stat
+
+import expr.Expr
+import symbols.Type
+import symbols.String
+import myvisitor.SymbolTable
+
+class Declaration(
+    val t: Type,
+    val id: String,
+    val rhs: AssignRhs,
+    st: SymbolTable
+) : Stat() {
+    init {
+        if (rhs.type == t) {
+            System.err.println("Expected " + t + ", got " + rhs.type)
+            valid = false
+        } else if (st.lookupAll(id) != null) {
+            System.err.println("Identifier " + id + " already defined")
+            valid = false
+        } else {
+            st.add(id, ???)
+        }
+    }
+}
