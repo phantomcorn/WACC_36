d6b0a97c0e79a114cd8aef76cbeaef9ac34cb4cb
Louis Aldous <la320>
2022-02-07 11:49:56 +0000

    feat: charLiteral implemented

--- src/symbols/CharLiteral.kt
+++ src/symbols/CharLiteral.kt
@@ -3,15 +3,25 @@ package symbols
 import kotlin.String
 
 class CharLiteral(value: String) : Expr(Char) {
-    var lit : kotlin.Char = '\u0000'
+    var lit: kotlin.Char = '\u0000'
+
     init {
         if (value.length == 1) {
             value.single().code in 0..127
         } else {
-            when(value){
-                "\\0", "\\b", "\\t", "\\n", "\\f", "\\r", "\\\"", "\\'", "\\\\" -> lit = ' '
+            // kotlin doesn't recognise some escape characters so unicode for these cases
+            when (value) {
+                "\\0" -> lit = '\u0000'
+                "\\b" -> lit = '\b'
+                "\\t" -> lit = '\t'
+                "\\n" -> lit = '\n'
+                "\\f" -> lit = '\u000c'
+                "\\r" -> lit = '\r'
+                "\\\"" -> lit = '\"'
+                "\\'" -> lit = '\''
+                "\\\\" -> lit = '\\'
                 else -> {
-                    valid = false;
+                    valid = false
                 }
             }
         }
