0bb15691318025ad1d49953426ddcdfb3d59d81f
Tao Wang <tw520@imperial.ac.uk>
2022-02-07 12:14:11 +0000

    feat: implemented check for NotEquiv binary operator

--- src/symbols/NotEquiv.kt
+++ src/symbols/NotEquiv.kt
@@ -1,13 +1,19 @@
 package symbols
 
 class NotEquiv(val e1: Expr, val e2: Expr) : BinaryOp(e1, e2, Boolean) {
-    override fun check(): kotlin.Boolean {
-        return (e1.type == e2.type) &&
-               (e1.type == Int ||
-                e1.type == Char ||
-                e1.type == Boolean ||
-                e1.type == String ||
-                e1.type is Array ||
-                e1.type is Pair)
+
+    init {
+        if (e1.type != e2.type) {
+            System.err.println("Mismatched expression types " + e1.type + " and " + e2.type)
+            valid = false
+        } else if (e1.type == Int ||
+            e1.type == Char ||
+            e1.type == Boolean ||
+            e1.type == String ||
+            e1.type is Array ||
+            e1.type is Pair) {
+            System.err.println("Invalid expression type " + e1.type)
+            valid = false
+        }
     }
 }
