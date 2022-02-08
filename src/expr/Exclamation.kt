4164e5216143bf2c409b24847fcd3c1fad97bd87
Tao Wang <tw520@ic.ac.uk>
2022-02-08 11:42:52 +0000

    feat: added symbol for exclamation unary operator

---
+++ src/expr/Exclamation.kt
@@ -0,0 +1,13 @@
+package expr
+
+import symbols.Boolean
+
+class Exclamation(e1: Expr) : UnaryOp(e1, Boolean) {
+
+    init {
+        if (e1.type != Boolean) {
+            System.err.println("Expected type int but actual type " + e1.type)
+            valid = false
+        }
+    }
+}
