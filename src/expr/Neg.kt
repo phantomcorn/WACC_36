bfdc24b45723f94b0f81205f9b0c2618a4c4061e
TaoWang <tw520@imperial.ac.uk>
2022-02-08 18:02:45 +0000

    feat: symbol for neg unary operator

---
+++ src/expr/Neg.kt
@@ -0,0 +1,13 @@
+package expr
+
+import symbols.Int
+
+class Neg(e1: Expr) : UnaryOp(e1, Int) {
+
+    init {
+        if (e1.type != Int) {
+            System.err.println("Expected type int but actual type " + e1.type)
+            valid = false
+        }
+    }
+}
