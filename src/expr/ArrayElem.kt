6fe8acaa2626e809b15b7b8923793efa5bda29e5
Charles Loveman <charlie@lovemans.co.uk>
2022-02-09 16:45:43 +0000

    fix: use correct type and check expr types

--- src/expr/ArrayElem.kt
+++ src/expr/ArrayElem.kt
@@ -2,15 +2,15 @@ package expr
 
 import stat.AssignLhs
 import symbols.Array
+import symbols.Int
 import symbols.Type
-import visitor.SymbolTable
 
 class ArrayElem(
     val id: kotlin.String,
     val values: kotlin.Array<Expr>,
     dims: kotlin.Int,
     t: Type?
-) : Expr(t), AssignLhs {
+) : Expr(t?.getBaseType()), AssignLhs {
     init {
         if (t == null) {
             System.err.println("Array $id not defined")
@@ -22,7 +22,12 @@ class ArrayElem(
             System.err.println("Dimension Error")
             valid = false
         } else {
-
+            for (e in values) {
+                if (!(e.type == Int)) {
+                    System.err.println("Expected Int, got " + e.type)
+                    valid = false
+                }
+            }
         }
     }
 }
