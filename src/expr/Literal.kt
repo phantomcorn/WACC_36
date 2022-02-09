2b2e0df57e7a8958bec3f9a01c91c0fba43bcf53
Charles Loveman <charlie@lovemans.co.uk>
2022-02-09 17:00:01 +0000

    fix: avoid infinite recursion in literal getter

--- src/expr/Literal.kt
+++ src/expr/Literal.kt
@@ -4,7 +4,5 @@ import symbols.Type
 
 abstract class Literal<T>(val t: Type) : Expr(t) {
     protected var value: T? = null
-        get() {
-            return value
-        }
+        get() = field
 }
