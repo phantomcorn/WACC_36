1ccdc52eb574a2bc527103b93ec81054be2d59c6
phantomcorn <jj.phantakorn@outlook.com>
2022-02-10 13:35:02 +0000

    feat : while error message complete

--- src/stat/While.kt
+++ src/stat/While.kt
@@ -7,7 +7,8 @@ import symbols.Identifier
 class While(val e: Expr, val s: Stat) : Stat() {
     init {
         if (e.type !is Boolean) {
-            System.err.println("While condition \"${e.toString()}\" does not evaluate to a Boolean. Got : ${e.type}")
+            ErrorHandler.printErr(ErrorType.SEMANTIC,
+                "While condition \"${e.toString()}\" does not evaluate to a Boolean. Got : ${e.type}")
             Identifier.valid = false
         }
     }
