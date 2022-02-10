429544a2e8a7b1864be114d170d3d33dfbb6e703
Charles Loveman <cel20@ic.ac.uk>
2022-02-10 13:50:01 +0000

    refactor: improve call error messages

--- src/stat/Call.kt
+++ src/stat/Call.kt
@@ -7,7 +7,7 @@ import symbols.Identifier
 import symbols.Type
 import visitor.SymbolTable
 
-class Call(val values: kotlin.Array<Expr>, id: kotlin.String, st: SymbolTable) : Identifier(), AssignRhs {
+class Call(val values: kotlin.Array<Expr>, val id: kotlin.String, st: SymbolTable) : Identifier(), AssignRhs {
     var type: Type? = null
 
     init {
@@ -23,30 +23,40 @@ class Call(val values: kotlin.Array<Expr>, id: kotlin.String, st: SymbolTable) :
             Identifier.valid = false
         } else if (func is Function) {
             if (func.params.values.size != values.size) {
-                System.err.println("Expected " + func.params.values.size + " args, got " + values.size)
+                ErrorHandler.printErr(ErrorType.SEMANTIC,
+                    "Incorrect number of parameters for $id (expected: "
+                    + func.params.values.size + ", actual: " + values.size + ")")
                 Identifier.valid = false
             } else {
                 type = func.returnType
                 for (i in 0..(func.params.values.size - 1)) {
                     if (values[i].type != func.params.values[i].paramType) {
                         Identifier.valid = false
-                        System.err.print("Expecting type : " + func.params.values[i].paramType)
-                        System.err.println(" but actual type: " + values[i].type)
+                        ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at " + values[i]
+                        + " (expected: " + func.params.values[i].paramType
+                        + ", actual: " + values[i].type + ")")
                     }
                 }
             }
         } else {
             val func = func as FuncType
             if (func.params.size != values.size) {
-                System.err.println("Expected " + func.params.size + " args, got " + values.size)
                 Identifier.valid = false
+                ErrorHandler.printErr(ErrorType.SEMANTIC,
+                    "Incorrect number of parameters for $id (expected: "
+                    + func.params.values.size + ", actual: " + values.size + ")")
+
             } else {
                 type = func.returnType
                 for (i in 0..(func.params.size - 1)) {
                     if (values[i].type != func.params[i]) {
                         Identifier.valid = false
-                        System.err.print("Expecting type : " + func.params[i])
-                        System.err.println(" but actual type: " + values[i].type)
+                        ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at " + values[i]
+                        + " (expected: " + func.params[i]
+                        + ", actual: " + values[i].type + ")")
+ 
                     }
                 }
             }
@@ -54,4 +64,15 @@ class Call(val values: kotlin.Array<Expr>, id: kotlin.String, st: SymbolTable) :
     }
 
     override fun type(): Type? = type
+
+    override fun toString(): kotlin.String {
+        var result = "call $id("
+        if (values.size > 0) {
+            result = result + values[0]
+            for (i in 1..(values.size - 1)) {
+                result = result + ", " + values[i]
+            }
+        }
+        return result + ")"
+    }
 }
