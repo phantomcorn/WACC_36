239aca80a3104315eb3c7505e2d5499a25d933df
Charles Loveman <cel20@ic.ac.uk>
2022-02-10 13:33:50 +0000

    refactor: improve assignment error messages

--- src/stat/Assignment.kt
+++ src/stat/Assignment.kt
@@ -5,8 +5,12 @@ import symbols.Identifier
 class Assignment(val lhs: AssignLhs, val rhs: AssignRhs) : Stat() {
     init {
         if (lhs.type() != rhs.type()) {
-            System.err.println("Expected " + lhs.type() + ", got " + rhs.type())
+            ErrorHandler.printErr(ErrorType.SEMANTIC,
+            "Incompatible type at $rhs (expected: " + lhs.type() +
+            ", actual: " + rhs.type() + ")")
             Identifier.valid = false
         }
     }
+
+    override fun toString() = "$lhs = $rhs" 
 }
