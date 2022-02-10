63d618c463c2b22ab0dcc52d98706a856e955801
Tao Wang <tw520@ic.ac.uk>
2022-02-10 12:42:58 +0000

    refactor: error messages and toString methods for binary operators

--- src/expr/Minus.kt
+++ src/expr/Minus.kt
@@ -7,15 +7,14 @@ class Minus(e1: Expr, e2: Expr) : BinaryOp(e1, e2, Int) {
 
     init {
         if (e1.type != Int) {
-            System.err.println("Expected type int but actual type " + e1.type)
+            ErrorHandler.printErr(ErrorType.SEMANTIC, "Incompatible type at " + this.toString() + " (expected: INT, actual: " + e1.type + ")")
             Identifier.valid = false
         } else if (e2.type != Int) {
-            System.err.println("Expected type int but actual type " + e2.type)
+            ErrorHandler.printErr(ErrorType.SEMANTIC, "Incompatible type at " + this.toString() + " (expected: INT, actual: " + e1.type + ")")
             Identifier.valid = false
         }
     }
 
-    override fun toString(): String {
-        return "${e1.toString()} - ${e2.toString()}"
-    }
+    override fun toString(): String =
+        "$e1-$e2"
 }
--- src/expr/Or.kt
+++ src/expr/Or.kt
@@ -4,13 +4,17 @@ import symbols.Boolean
 import symbols.Identifier
 
 class Or(e1: Expr, e2: Expr) : BinaryOp(e1, e2, Boolean) {
+
     init {
         if (e1.type != Boolean) {
-            System.err.println("Expected type int but actual type " + e1.type)
+            ErrorHandler.printErr(ErrorType.SEMANTIC, "Incompatible type at " + this.toString() + " (expected: BOOL, actual: " + e1.type + ")")
             Identifier.valid = false
         } else if (e2.type != Boolean) {
-            System.err.println("Expected type int but actual type " + e2.type)
+            ErrorHandler.printErr(ErrorType.SEMANTIC, "Incompatible type at " + this.toString() + " (expected: BOOL, actual: " + e2.type + ")")
             Identifier.valid = false
         }
     }
+
+    override fun toString() : String =
+        "$e1||$e2"
 }
