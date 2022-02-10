8b231646609af7197907d61e9ae0c45b9d6843f1
Charles Loveman <charlie@lovemans.co.uk>
2022-02-10 15:14:05 +0000

    refactor: improve exit error messages

--- src/stat/Exit.kt
+++ src/stat/Exit.kt
@@ -7,7 +7,10 @@ import symbols.Int
 class Exit(val e: Expr) : Stat() {
     init {
         if (!(e.type is Int)) {
-            System.err.println("Exit expected Int, got " + e.type)
+            ErrorHandler.printErr(
+                ErrorType.SEMANTIC,
+                "Incompatible type at $e (expected: Int, actual: " + e.type + ")"
+            )
             Identifier.valid = false
         }
     }
