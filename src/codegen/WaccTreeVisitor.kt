dc60417979c39ffc901930304553327359257777
Tao Wang <tw520@ic.ac.uk>
2022-02-15 15:14:03 +0000

    feat: WaccTreeVisitor class with methods for all AST nodes

---
+++ src/codegen/WaccTreeVisitor.kt
@@ -0,0 +1,213 @@
+package codegen
+
+import instr.Instruction
+
+class WaccTreeVisitor : ASTVisitor {
+    override fun visitAST() {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitSkipNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitWhileNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitDeclarationNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitAssignmentNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitReadNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitExitNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPrintNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPrintlnNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitSemiNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitFreeNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitIfNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitBeginNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitReturnNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitVariableNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitNewPairNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitCallNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitArrayLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitEmptyArrayLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitArrayInstanceNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitIntNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitBooleanNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitCharNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitStringNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitTypelessPairNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPairInstanceNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitArgListNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPairElemNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitArrayElemNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPairLiteral(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitOrNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitAndNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitEquivNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitNotEquivNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitGtNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitGteNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitLtNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitLteNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitPlusNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitMinusNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitMultiNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitDivNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitModNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitIntLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitCharLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitBooleanLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitStringLiteralNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitNotNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitNegNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitLenNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitOrdNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitChrNode(): List<Instruction> {
+        TODO("Not yet implemented")
+    }
+}
