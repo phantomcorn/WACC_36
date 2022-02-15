f6f63809f2f5732dfcc3b4e0a7876f8bff9a0a95
Charles Loveman <cel20@ic.ac.uk>
2022-02-15 14:36:51 +0000

    feat: make ASTNode inherit Identifier

--- src/expr/Expr.kt
+++ src/expr/Expr.kt
@@ -1,9 +1,10 @@
 package expr
 
+import codegen.ASTNode
 import stat.AssignRhs
 import symbols.Identifier
 import symbols.Type
 
-abstract class Expr(val type: Type?) : Identifier(), AssignRhs {
+abstract class Expr(val type: Type?) : ASTNode(), AssignRhs {
     override fun type() : Type? = type
 }
