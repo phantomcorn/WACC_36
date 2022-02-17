25633bda83e8a7f0ff98491d4843c6ae3fff775b
Charles Loveman <cel20@ic.ac.uk>
2022-02-17 11:22:23 +0000

    feat: add register package to makefile

--- src/instr/InstructionVisitor.kt
+++ src/instr/InstructionVisitor.kt
@@ -5,6 +5,8 @@ interface InstructionVisitor {
 
     fun visitTest(rn: Register, op2: Operand2): String
 
+    fun visitTestEquiv(rn: Register, op2: Operand2): String
+
     fun visitAnd(rd: Register, rn: Register, op2: Operand2): String
 
     fun visitXor(rd: Register, rn: Register, op2: Operand2): String
---
+++ src/instr/TestEquiv.kt
@@ -0,0 +1,7 @@
+package instr
+
+class TestEquiv(val Rn: Register, val Op2: Operand2) : Instruction {
+    override fun accept(v: InstructionVisitor): String {
+        return v.visitTestEquiv(Rn, Op2)
+    }
+}
