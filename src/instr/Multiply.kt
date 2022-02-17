639e46ec079f2c564866507d26efec5ffd266cd9
Louis Aldous <la320>
2022-02-17 12:01:00 +0000

    fix: missing params in Multiply

--- src/instr/Multiply.kt
+++ src/instr/Multiply.kt
@@ -2,7 +2,8 @@ package instr
 
 import register.Register
 
-class Multiply(val Rd : Register, val Rm : Register, val Rs: Register) : Instruction() {
+class Multiply(val Rd: Register, val Rm: Register, val Rs: Register, cond: Cond = Cond.AL, s: Boolean = false) :
+    Instruction(cond, s) {
     override fun accept(v: InstructionVisitor): String {
         return v.visitMul(Rd, Rm, Rs)
     }
