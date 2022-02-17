709ee49201f07edca842e5e7438ae3d3ce77e50e
Louis Aldous <la320>
2022-02-17 11:39:59 +0000

    feat: implemented subtract instruction

---
+++ src/instr/Subtract.kt
@@ -0,0 +1,15 @@
+package instr;
+
+import register.Register;
+
+class Subtract(
+    val Rd: Register,
+    val Rn: Register,
+    val operand2: Operand2,
+    cond: Cond = Cond.AL,
+    s: Boolean = false
+) : Instruction(cond, s) {
+    override fun accept(v: InstructionVisitor): String {
+        return v.visitSub(Rd, Rn, operand2)
+    }
+}
