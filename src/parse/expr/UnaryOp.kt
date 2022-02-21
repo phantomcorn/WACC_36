631411635670d54448fa346649747171eda7fdd5
Tao Wang <tw520@ic.ac.uk>
2022-02-21 11:51:58 +0000

    feat: moved all unary operators into single enum class to significantly reduce total number of classes

--- src/parse/expr/UnaryOp.kt
+++ src/parse/expr/UnaryOp.kt
@@ -1,5 +1,47 @@
 package parse.expr
 
-import parse.symbols.Type
+import codegen.ASTVisitor
+import codegen.instr.Instruction
+import parse.symbols.*
 
-abstract class UnaryOp(val e: Expr, t: Type) : Expr(t)
+class UnaryOp(val e: Expr, t: Type, private val op: UnaryOperator) : Expr(t) {
+
+    init {
+        when (op) {
+            UnaryOperator.CHR,
+            UnaryOperator.NEG -> {
+                if (e.type != Int) {
+                    ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at $this (expected: INT, actual ${e.type})")
+                }
+            }
+            UnaryOperator.LEN -> {
+                if (e.type !is Array) {
+                    ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at $this (expected: ARRAY, actual ${e.type}")
+                }
+            }
+            UnaryOperator.ORD -> {
+                if (e.type != Char) {
+                    ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at $this (expected: CHAR, actual ${e.type})")
+                }
+            }
+            UnaryOperator.NOT -> {
+                if (e.type != Boolean) {
+                    ErrorHandler.printErr(ErrorType.SEMANTIC,
+                        "Incompatible type at $e (expected: BOOLEAN, actual ${e.type})")
+                }
+            }
+        }
+    }
+
+    override fun accept(v: ASTVisitor): List<Instruction> {
+        return v.visitUnaryOpNode(this)
+    }
+
+    override fun toString(): String {
+        return "$op $e"
+    }
+}
+
---
+++ src/parse/expr/UnaryOperator.kt
@@ -0,0 +1,3 @@
+package parse.expr
+
+import parse.symbols.
