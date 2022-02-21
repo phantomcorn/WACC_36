d8a8859b160f454e4f3a16df279c35f9f02433d0
Tao Wang <tw520@ic.ac.uk>
2022-02-21 11:59:15 +0000

    feat: add enum class for unary operators to reduce total number of classes required

--- src/parse/expr/UnaryOperator.kt
+++ src/parse/expr/UnaryOperator.kt
@@ -1,3 +1,16 @@
 package parse.expr
 
-import parse.symbols.
+import parse.symbols.Boolean
+import parse.symbols.Char
+import parse.symbols.Int
+import parse.symbols.Type
+
+enum class UnaryOperator(t: Type) {
+    CHR(Int) { override fun toString(): String = "chr" },
+    LEN(Int) { override fun toString(): String = "len" },
+    ORD(Char) { override fun toString(): String = "ord" },
+    NEG(Int) { override fun toString(): String = "neg" },
+    NOT(Boolean) { override fun toString(): String = "not" };
+
+    abstract override fun toString() : String
+}
