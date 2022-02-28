1ac01f893eabd87aa3700f1f290d0de81ebc9436
Louis Aldous <la320>
2022-02-28 10:49:37 +0000

    fix: ImmediateOffset use Int instead of Immediate

--- src/codegen/instr/operand2/ImmediateOffset.kt
+++ src/codegen/instr/operand2/ImmediateOffset.kt
@@ -4,7 +4,7 @@ import codegen.instr.InstructionVisitor
 import codegen.instr.loadable.Loadable
 import codegen.instr.register.Register
 
-class ImmediateOffset(val r: Register, val value: Int) : Operand2, Loadable {
+class ImmediateOffset(val r: Register, val value: Immediate) : Operand2, Loadable {
     override fun accept(v: InstructionVisitor): String {
         return v.visitImmediateOffset(this)
     }
