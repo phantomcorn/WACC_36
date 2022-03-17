b1a3123760de85c1b4ab36c5330da78be3c397bb
Charles Loveman <cel20@ic.ac.uk>
2022-03-17 17:58:26 +0000

    Fix: Add toString to Generic to avoid stack overflow.

--- src/parse/symbols/Generic.kt
+++ src/parse/symbols/Generic.kt
@@ -1,9 +1,12 @@
 package parse.symbols
 
 import kotlin.Int
+import kotlin.String
 
 object Generic : Type() {
     override fun getByteSize(): Int {
         throw Exception("called getByteSize on uninstantiated generic type")
     }
+
+    override fun toString(): String = "Generic"
 }
